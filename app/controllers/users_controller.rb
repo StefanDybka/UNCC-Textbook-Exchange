77b14b8088419dd36df924274c5c772062e8337c
Stefan Dybka <sdybka@uncc.edu>
2019-04-09 22:28:12 +0000

    Added show to User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,19 @@ class UsersController < ApplicationController
     def new
     end
     
+    def show
+        @user = User.find(params[:id])
+    end
+    
     def create
-        render plain: params[:user].inspect
+        @user = User.new(user_params)
+        
+        @user.save
+        redirect_to @user
     end
 end
+
+private
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
