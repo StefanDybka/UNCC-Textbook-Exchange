1fbae4b824f9b68f522039f64281f778313f9c3d
Stefan Dybka <sdybka@uncc.edu>
2019-04-11 12:50:45 +0000

    Fixed the listing links to point to the listing pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_09_203041) do
+ActiveRecord::Schema.define(version: 2019_04_09_205339) do
 
   create_table "listings", force: :cascade do |t|
     t.string "name"
     t.integer "isbn"
     t.string "email"
     t.string "condition"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
