fb078094716cfa1a73fbfcde740eb399090864ac
Oliver King <oking3@uncc.edu>
2019-04-12 04:01:53 +0000

    Added model verification for listings and added back buttons

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,14 +1,22 @@
 class ListingsController < ApplicationController
+    
+    before_action :check_cancel, :only => [:create]
+    
     def new
+        @listing = Listing.new
     end
+    
     def index
         @listings = Listing.all
     end
     def create
         @listing = Listing.new(listing_params)
         
-        @listing.save
-        redirect_to @listing
+        if @listing.save
+            redirect_to @listing
+        else
+            render 'new'
+        end
     end
     def show
         @listing = Listing.find(params[:id])
@@ -19,4 +27,10 @@ end
 private 
     def listing_params
         params.require(:listing).permit(:name, :isbn, :email, :condition)
+    end
+
+    def check_cancel
+        if params[:commit] == "Back"
+            redirect_to root_url
+        end
     end
