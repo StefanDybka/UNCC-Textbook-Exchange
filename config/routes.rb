e367b7087cb62727e70f33bda8fd898b892fc335
Stefan Dybka <sdybka@uncc.edu>
2019-04-15 21:02:07 +0000

    Revert "Added a help page and set up a static route for it"
    This reverts commit 91c73dd3c2d49af4d0d51b8cc6d8beb3cceb87a1.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/index'
   get '/about', to: 'welcome#about'
-  get '/help', to: 'welcome#help'
   get 'users/new'
 
   resources :listings
