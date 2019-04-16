d2cee7472e8dce37a2d0cac00355875564e6c988
Josh Williams <jwill564@uncc.edu>
2019-04-16 03:24:02 +0000

    Set up About page

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   get 'users/new'
   root 'welcome#index'
   get 'welcome/index'
-  get     '/help', to: 'help#show'
-
+  get '/help', to: 'help#show'
+  get '/about', to: 'about#show'
+  
   resources :listings
   resources :users
+  resources :help
+  resources :about
 
 end
