668c81768a044fe114beed9873a3c676ef6a1d3e
Stefan Dybka <sdybka@uncc.edu>
2019-04-16 21:38:30 +0000

    Refactored account activation to the model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end  
   
   def new
@@ -11,7 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_path
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,16 @@ class User < ApplicationRecord
         return false if digest.nil?
         BCrypt::Password.new(digest).is_password?(token)
     end
+    
+    # Activates an account
+    def activate
+        update_columns(activated: true, activated_at: Time.zone.now)
+    end
+    
+    # Sends activation email
+    def send_activation_email
+        UserMailer.account_activation(self).deliver_now
+    end
 
     private
         # Converts email to all lower-case
