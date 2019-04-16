c7a0a99bce1fb0b4ef7fc35aea85a055349ca772
Stefan Dybka <sdybka@uncc.edu>
2019-04-16 19:11:08 +0000

    Added edit functionality to user authentication

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,15 @@
 class AccountActivationsController < ApplicationController
+    def edit
+        user = User.find_by(email: params[:email])
+        if user && !user.activated? && user.authenticated?(:activation, params[:id])
+            user.update_attribute(:activated, true)
+            user.update_attribute(:activated_at, Time.zone.now)
+            log_in user
+            flash[:sucess] = "Account activated!"
+            redirect_to user
+        else
+            flash[:dange] = "Invalid actication link"
+            redirect_to root_path
+        end
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = "Account not activated. Ceck your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_path
+      end
     else
-      # Invalid combination
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
