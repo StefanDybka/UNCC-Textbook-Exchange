8fd7153c4b1f4a07e04648341b13da9a99c087af
Stefan Dybka <sdybka@uncc.edu>
2019-04-16 14:41:05 +0000

    Added the ability to remember logged-in users with cookies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_14_212223) do
+ActiveRecord::Schema.define(version: 2019_04_16_130046) do
 
   create_table "listings", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_04_14_212223) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
