85519426fa28e47abc1ad39c293e82c55b24cd23
Josh Williams <jwill564@uncc.edu>
2019-04-23 20:01:50 +0000

    fixed static page routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/index'
 
-  get '/help', to: 'help#show'
-  get '/about', to: 'about#show'
+  get '/help', to: 'welcome#help'
   get '/about', to: 'welcome#about'
   get '/signup',to: 'users#new'
   post'/signup',to: 'users#create'
-  get '/help',  to: 'help#show'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
