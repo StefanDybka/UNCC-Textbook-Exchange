21eea462e14d6a37af516021efd416e21a334b0e
Stefan Dybka <sdybka@uncc.edu>
2019-04-23 20:05:13 +0000

    Fixed help and about routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/index'
 
-  get '/help', to: 'help#show'
-  get '/about', to: 'about#show'
+  get '/help',  to: 'welcome#help'
   get '/about', to: 'welcome#about'
   get '/signup',to: 'users#new'
   post'/signup',to: 'users#create'
-  get '/help',  to: 'help#show'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
@@ -17,8 +15,6 @@ Rails.application.routes.draw do
   resources :listings
   resources :users
   resources :reports
-  resources :help
-  resources :about
   resources :account_activations, only: [:edit]
 
 end
