ea85d853757c98c7386cece73972def86164c392
Oliver King <oking3@uncc.edu>
2019-04-24 02:10:31 +0000

    Styled user show page edit column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_170355) do
+ActiveRecord::Schema.define(version: 2019_04_23_200532) do
 
   create_table "listings", force: :cascade do |t|
     t.string "title"
