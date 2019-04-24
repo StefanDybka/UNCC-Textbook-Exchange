0c1ac4cd7297105c21a2f7b95fca71f8985ba0fc
Stefan Dybka <sdybka@uncc.edu>
2019-04-24 14:31:07 +0000

    Added password reset digest to users and new password reset page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :listings
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     before_save :downcase_email
     before_create :create_activation_digest
     validates :fname, presence: true, length: { maximum: 50, minimum: 4}
@@ -51,6 +51,11 @@ class User < ApplicationRecord
         UserMailer.account_activation(self).deliver_now
     end
 
+    # Sends reset email
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
+    
     private
         # Converts email to all lower-case
         def downcase_email
@@ -61,5 +66,12 @@ class User < ApplicationRecord
         def create_activation_digest
             self.activation_token = User.new_token
             self.activation_digest = User.digest(activation_token)
+        end
+        
+        # Creates and assigns a password reset token and digest
+        def create_reset_digest
+            self.reset_token = User.new_token
+            update_attribute(:reset_digest, User.digest(reset_token))
+            update_attribute(:reset_sent_at, Time.zone.now)
         end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_200532) do
+ActiveRecord::Schema.define(version: 2019_04_23_222642) do
 
   create_table "listings", force: :cascade do |t|
     t.string "title"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2019_04_23_200532) do
     t.datetime "activated_at"
     t.string "lname"
     t.boolean "admin", default: false
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
