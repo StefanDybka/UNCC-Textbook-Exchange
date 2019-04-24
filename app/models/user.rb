898fdcdd27c9c7e67aff7278a84ab81d22449260
Stefan Dybka <sdybka@uncc.edu>
2019-04-24 15:08:24 +0000

    Finished up password reset emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,6 +56,18 @@ class User < ApplicationRecord
         UserMailer.password_reset(self).deliver_now
     end
     
+    # Returns true if a password reset has expired.
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
+    end
+    
+     
+    # Creates and assigns a password reset token and digest
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+    end
+    
     private
         # Converts email to all lower-case
         def downcase_email
@@ -66,12 +78,5 @@ class User < ApplicationRecord
         def create_activation_digest
             self.activation_token = User.new_token
             self.activation_digest = User.digest(activation_token)
-        end
-        
-        # Creates and assigns a password reset token and digest
-        def create_reset_digest
-            self.reset_token = User.new_token
-            update_attribute(:reset_digest, User.digest(reset_token))
-            update_attribute(:reset_sent_at, Time.zone.now)
         end
 end
