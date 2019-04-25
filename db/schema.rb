87e49fa21d3ac71acee44ee7ca00cef48fa838c5
Stefan Dybka <sdybka@uncc.edu>
2019-04-25 12:54:29 +0000

    Removed Conditions database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_222642) do
+ActiveRecord::Schema.define(version: 2019_04_25_125048) do
 
   create_table "listings", force: :cascade do |t|
     t.string "title"
