136e734b9c107c3d01e7e281105494b7a40a2ad3
Oliver King <oking3@uncc.edu>
2019-04-28 19:04:58 +0000

    Fixed ISBN regex to allow for blanks and updated en.yml with isbn

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,7 +4,7 @@ class Listing < ApplicationRecord
 
     validates :title, presence: true, length: { minimum: 4 }
     validates :email, presence: true, length: { minimum: 4 }
-    ISBN_REGEX = /\A(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+\z/
+    ISBN_REGEX = /(\A(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d-]+\z)|(\A\z)/
     validates :isbn, format: { with: ISBN_REGEX }
     validates :condition, presence: true
 
